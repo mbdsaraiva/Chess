fdc166a8541e4915dd980d7c132b83063533c8e0
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-03-30 20:16:31 -0300

    queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -12,6 +12,7 @@ import chess.pieces.Bishop;
 import chess.pieces.Horse;
 import chess.pieces.King;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -194,6 +195,7 @@ public class ChessMatch {
  	}
  	
  	private void initialSetup() {
+ 		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
  		placeNewPiece('b', 1, new Horse(board, Color.WHITE));
  		placeNewPiece('g', 1, new Horse(board, Color.WHITE));
  		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
@@ -210,6 +212,7 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('b', 8, new Horse(board, Color.BLACK));
         placeNewPiece('g', 8, new Horse(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
