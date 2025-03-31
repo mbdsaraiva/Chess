964b35f53428bdcc73cf75e21dcd504d594af051
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-03-31 01:04:12 -0300

    fix

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -128,15 +127,17 @@ public class ChessMatch {
  		}
  		if (!type.equals("B") && !type.equals("H") &&!type.equals("R") && !type.equals("Q")) {
  			
- 			throw new InvalidParameterException("Invalid type for promotion");
+ 			return promoted;
  		}
  		
  		Position pos = promoted.getChessPosition().toPosition();
  		Piece p =  board.removePiece(pos);
  		piecesOnTheBoard.remove(p);
+ 		
  		ChessPiece newPiece = newPiece(type, promoted.getColor());
  		board.placePiece(newPiece, pos);
  		piecesOnTheBoard.add(newPiece);
+ 		
  		return newPiece;
  	}
  	
